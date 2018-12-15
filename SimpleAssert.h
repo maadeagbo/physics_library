0f152678c6bb97fa85f017b01148cdd734df5308
Moses <maadeagbo@runbox.com>
2018-12-15 12:37:52 -0500

    Modified assert macro to be more lean && respect NDEBUG

--- SimpleAssert.h
+++ SimpleAssert.h
@@ -5,21 +5,34 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#include <assert.h>
+
+#ifdef _WIN32
+#define SIMPLE_HALT() __debugbreak()
+#else
+#define SIMPLE_HALT() __builtin_trap();
+#endif  // linux
 
 #define clean_errno() ( errno == 0 ? "None" : strerror( errno ) )
 
-#define log_error( M, ... )              \
-    fprintf( stdout,                     \
-             "%s:%d: errno: %s " M "\n", \
-             __FILE__,                   \
-             __LINE__,                   \
-             clean_errno(),              \
+#define log_error( M, ... )                     \
+    fprintf( stderr,                            \
+             "%s : %d\n  errno: %s\n  " M "\n", \
+             __FILE__,                          \
+             __LINE__,                          \
+             clean_errno(),                     \
              ##__VA_ARGS__ )
+
+#ifdef NDEBUG
+
+#define assertf()
+
+#else
 
 #define assertf( A, M, ... )           \
     if( !( A ) )                       \
     {                                  \
         log_error( M, ##__VA_ARGS__ ); \
-        assert( A );                   \
+        SIMPLE_HALT();                 \
     }
+
+#endif  // NDEBUG
